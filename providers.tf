d2779d996999e63e33cd0df29569944e2dea1ce8
ANSHUMAAN SINGH <124035463+anshumaan-10@users.noreply.github.com>
2024-11-21 12:15:57 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,17 +1,14 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "3.85.0"
     }
   }
 }
 
 provider "google" {
-  # Configuration options
   project = "avian-voice-433417-d5"
-  region = "us-central1"
-  zone = "us-central1-a"
-  credentials = "keys.json"
+  region  = "us-central1"
+  zone    = "asia-southeast1-a"
 }
-
