658bea7465690c236a07c7342a0ec33c8a7dde54
ANSHUMAAN SINGH <124035463+anshumaan-10@users.noreply.github.com>
2024-11-21 11:55:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,61 +1,68 @@
-# Define a Google Compute Engine instance named "vm-from-tf"
-resource "google_compute_instance" "vm-from-tf" {
-  name = "vm-from-tf"
-  zone = "asia-southeast1-a"
-  machine_type = "n1-standard-1"
+provider "google" {
+  project     = "avian-voice-433417-d5"
+  region      = "us-central1"
+  zone        = "asia-southeast1-a"  # Use the same zone here
+  credentials = "keys.json"  # Pass this securely in GitHub Actions
+}
 
+resource "google_compute_instance" "vm-from-tf" {
+  name           = "vm-from-tf"
+  zone           = "asia-southeast1-a"
+  machine_type   = "n1-standard-1"
   allow_stopping_for_update = true
 
-  # Define network interface settings
   network_interface {
-    network = "default"
+    network    = "default"
     subnetwork = "default"
   }
 
-  # Define boot disk settings
   boot_disk {
     initialize_params {
-      image = "debian-9-stretch-v20210916"  # Debian 9 Stretch image
-      size = 35  # Disk size: 35 GB
+      image = "debian-9-stretch-v20210916"
+      size  = 35
     }
     auto_delete = false
   }
 
-  # Define labels for the instance
   labels = {
-    "env" = "tflearning"  # Environment label: "tfleaning"
+    "env" = "tflearning"
   }
 
-  # Define scheduling parameters
   scheduling {
-    preemptible = false  # Not preemptible
-    automatic_restart = false  # Do not automatically restart
-  }
-  
-  # Define service account settings
-  service_account {
-    email = "815278883793-compute@developer.gserviceaccount.com"
-    scopes = [ "cloud-platform" ]  # Cloud Platform scope
+    preemptible       = false
+    automatic_restart = false
   }
 
-  # Define lifecycle configuration to ignore changes to attached disks
+  service_account {
+    email  = "815278883793-compute@developer.gserviceaccount.com"
+    scopes = ["cloud-platform"]
+  }
+
   lifecycle {
     ignore_changes = [
-      attached_disk
+      boot_disk,
+      network_interface
     ]
+  }
+
+  attached_disk {
+    source      = google_compute_disk.disk-1.id
+    device_name = "disk-1"
   }
 }
 
-# Define a Google Compute Engine disk named "disk-1"
 resource "google_compute_disk" "disk-1" {
-  name = "disk-1"
-  size = 15  # Disk size: 15 GB
-  zone = "asia-southeast1-a"
-  type = "pd-ssd"  # Disk type: SSD
+  name   = "disk-1"
+  size   = 15
+  zone   = "asia-southeast1-a"
+  type   = "pd-ssd"
 }
 
-# Attach the disk to the instance
-resource "google_compute_attached_disk" "adisk" {
-  disk = google_compute_disk.disk-1.id  # Attach disk-1
-  instance = google_compute_instance.vm-from-tf.id  # to the vm-from-tf instance
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.85.0"
+    }
+  }
 }
